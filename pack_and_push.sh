a90e99a6d0a9e3670e21d76adbf796006fce505c
kipperedsnack <20378331+KipperedSnack@users.noreply.github.com>
2018-06-21 06:57:01 -0500

    Updated to fix things...

--- pack_and_push.sh
+++ pack_and_push.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+VERSION=$(echo ./launcher/build/libs/launcher-*-all.jar | cut -d- -f2)
 pack200 --no-gzip $(echo ./launcher/build/libs/launcher-*-all.jar | cut -d- -f2).pack ./launcher/build/libs/launcher-*-all.jar
 scp -P 2202 $(echo ./launcher/build/libs/launcher-*-all.jar | cut -d- -f2).pack root@worldautomation.net:/storage/launcher/versions/.
+ssh -p 2202 root@worldautomation.net "rm /storage/launcher/latest.json;sed 's/UPDATE/$VERSION/g' /storage/launcher/template.json >> /storage/launcher/latest.json"
+#This would update the actual modpack, but not needed here... we use this as a PHP script on the WA-Mod-Pack repo now.
 #ssh -p 2202 root@worldautomation.net "/storage/launcher/creator.sh"
