a111e5f7a6c3e6fcca153673208ef224af70ff69
resist-network <resist_network@protonmail.com>
2018-10-08 06:51:35 -0500

    Fixed path...

--- pack_and_push.sh
+++ pack_and_push.sh
@@ -6,3 +6,4 @@ scp -P 2202 $(echo ./launcher/build/libs/launcher-*-all.jar | cut -d- -f2).pack 
 ssh -p 2202 root@worldautomation.net "rm /storage/WA-Web-Pack/public/launcher/latest.json;sed 's/UPDATE/$VERSION/g' /storage/WA-Web-Pack/public/launcher/template.json >> /storage/WA-Web-Pack/public/launcher/latest.json"
 #This would update the actual modpack, but not needed here... we use this as a PHP script on the WA-Mod-Pack repo now.
 #ssh -p 2202 root@worldautomation.net "/storage/launcher/creator.sh"
+rm $(echo ./launcher/build/libs/launcher-*-all.jar | cut -d- -f2).pack
